ea21f958568946290bedfa54c4e30f38a6437543
detne <ngocquyensn204@gmail.com>
2025-05-24 14:57:52 +0700

    Update structure

---
+++ src/main/java/com/danang_auction/DanangAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.danang_auction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanangAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanangAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danang_auction/DanangAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danang_auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanangAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
